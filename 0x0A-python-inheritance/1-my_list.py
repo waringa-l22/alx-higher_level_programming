468be72599b00bbea8fbb957b12de11d69824f26
waringa-l22 <lwaringa22@gmail.com>
2022-10-27 04:29:06 +0300

    Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 class MyList(list):
     """ Class that inherits the attributes references of class list
+    
     Args:
         list: class list
+    
     """
 
     def print_sorted(self):
